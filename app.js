75c657182100872a94a035d020f3ce849400dcba
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-02-01 21:25:41 +0300

    8 - WeakSet

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 'use strict';
+
 let a = { a: 1 };
-let b = { a: 1 };
+let b = { b: 2 };
 
-const map = new WeakMap();
-map.set(a, 'testA');
-map.set(b, 'testB');
-
-console.log(map.get(a));
-console.log(map.has(a));
-console.log(map);
-
+const set = new WeakSet([a, b]);
 a = null;
 
 setTimeout(() => {
-	console.log(map);
-}, 1500);
-
-let cache = new WeakMap();
-
-function getValue(obj) {
-	if (!cache.has(obj)) {
-		const res = 1; /* Типа сложный расчет */
-		cache.set(obj, res);
-	}
-	return cache.get(obj);
-}
-
-const res = getValue(b);
-console.log(res);
-const res2 = getValue(b);
-console.log(res2);
+	console.log(set), 1500;
+});
