b7d9f6ac68310bcf621e2b2be40d5a56a096f4a4
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-02-03 20:11:00 +0300

    15 - Работа с BigInt

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 'use strict';
 
-const bigNum1 = 350_500_000;
-const bigNum2 = 350500000;
+const max = 2 ** 53 - 1;
 
-const payment = 20_10;
-const paymentInR1 = 20.1;
-const paymentInR2 = 2_0.1_0;
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
 
-console.log(Number('350_500_000'));
-console.log(Number('350500000'));
-console.log(Number.parseInt('350_500_000'));
-console.log(Number.parseFloat('350_500_000.10'));
+console.log(max);
+console.log(max + 1);
+console.log(max + 2);
+console.log(max + 3);
+
+console.log(1321321321321321321321321n);
+console.log(BigInt(1321321321321321321321321)); // некорректно, нужна строка
+console.log(BigInt('1321321321321321321321321'));
+
+console.log(10n + 10n);
+console.log(10n + BigInt(10));
+console.log(10n * 10n);
+console.log(1321321321321321321321321n * 1321321321321321321321321n);
+
+console.log(10n / 3n);
+
+console.log(10n < 20);
+console.log(10n == 10);
+console.log(10n === 10);
+console.log(typeof 10n);
