8bf250f8cf85f78f8b4f219ba184ec5ad0333059
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-20 21:49:02 +0300

    47 - Принцип разделения интерфейса

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 'use strict';
 
-class User {
-	#role = 'user';
-	getRole() {
-		return this.#role;
+class Weapon {
+	cost;
+
+	dealDamage() {}
+}
+
+class Rifle extends Weapon {
+	shoot() {
+		this.dealDamage();
 	}
 }
 
-class Admin extends User {
-	#role = ['user', 'admin'];
-	getRole() {
-		return this.#role.join(', ');
+class Sword extends Weapon {
+	strike() {
+		this.dealDamage();
 	}
 }
-
-function logRole(user) {
-	console.log('Role: ' + user.getRole().toUpperCase());
-}
-
-logRole(new User());
-logRole(new Admin());
