9c1da0d0dbba901e3886a883bb9cc2465a817bdd
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-21 20:30:32 +0300

    52 - Promises и fetch

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
 'use strict';
 
-/*
-Получить среднюю цену 30 товаров из API
-https://dummyjson.com/products
-*/
-
-const request = new XMLHttpRequest();
-request.open('GET', 'https://dummyjson.com/products');
-request.send();
-
-request.addEventListener('load', function () {
-	const { products } = JSON.parse(this.responseText);
-	console.log(products);
-
-	const request = new XMLHttpRequest();
-	request.open('GET', 'https://dummyjson.com/products/' + products[0].id);
-	request.send();
-
-	request.addEventListener('load', function () {
-		const data = JSON.parse(this.responseText);
-		console.log(data);
-	});
-});
+const res = fetch('https://dummyjson.com/products/1');
+console.log('res:', res);
