46d6ae619ff12f0996ca77d70ad50f4040ab93de
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-23 12:43:06 +0300

    62 - Статические методы Promise

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 'use strict';
 
-const prom = new Promise((resolve, reject) => {
-	if (new Date() < new Date('01/01/2025')) {
-		reject(new Error('Error'));
-	}
-	resolve('Success');
+const prom = new Promise(resolve => {
+	console.log('Constructor');
+	// for (let i = 0; i < 10000000000; i++) {}
+	setTimeout(() => {
+		resolve('Timer');
+	}, 1000);
 });
 
-prom.then(data => console.log(data)).catch(error => console.log(error));
+prom.then(data => console.log(data));
 
-function timeout(sec) {
-	return new Promise(resolve => {
-		setTimeout(() => {
-			resolve();
-		}, sec * 1000);
-	});
-}
-
-timeout(1)
-	.then(() => {
-		console.log(1);
-		return timeout(1);
-	})
-	.then(() => {
-		console.log(1);
-		return timeout(1);
-	})
-	.then(() => {
-		console.log(1);
-		return timeout(1);
-	})
-	.then(() => {
-		console.log(1);
-		return timeout(1);
-	})
-	.then(() => {
-		console.log(1);
-		return timeout(1);
-	});
+Promise.reject(new Error('Error')).catch(error => console.error(error));
+Promise.resolve('Instant').then(data => console.log(data));
