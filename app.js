4265ccd46c24a963e1d6182b2d26d5e063571ffa
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-24 20:59:15 +0300

    74 - Визуальное положение элементов

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 'use strict';
 
-console.log(document.head);
-console.log(document.body);
-
-const el1 = document.querySelector('.wrapper');
-const el2 = document.querySelectorAll('meta');
-console.log('el2:', el2);
-console.log('el1:', el1);
-const el3 = document.getElementsByClassName('wrapper');
-const el4 = document.getElementsByTagName('meta');
-console.log('el3:', el3);
-console.log('el4:', el4);
-
-const button1 = document.createElement('button');
-button1.innerText = 'test1';
-
-const button2 = document.createElement('button');
-button2.innerText = 'test2';
-
-el1.append(button1);
-// el1.prepend(button2);
-// el1.before(button2);
-el1.after(button2);
-
 function generate() {
-	el1.remove();
+	console.log(`X offset: ${window.pageXOffset}`);
+	console.log(`Y offset: ${window.pageYOffset}`);
+	console.log(`clientWidth: ${document.documentElement.clientWidth}`);
+	console.log(`clientHeight: ${document.documentElement.clientHeight}`);
+
+	const el = document.querySelector('.down');
+	const rect = el.getBoundingClientRect();
+
+	window.scrollTo({
+		left: window.pageYOffset + rect.left,
+		top: window.pageYOffset + rect.top,
+		behavior: 'smooth',
+	});
 }
