ea7e805fd9ff12d23f0015f0f4508f0683346efb
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-24 20:40:07 +0300

    73 - Работа с элементами

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 'use strict';
 
-/*
-		Сделать генератор 3х идей от скуки
-		https://www.boredapi.com/api/activity
-		с отображением на странице
-*/
-const wrapper = document.querySelector('.wrapper');
+console.log(document.head);
+console.log(document.body);
 
-async function getActivity() {
-	const res = await fetch('https://www.boredapi.com/api/activity');
-	return res.json();
-}
+const el1 = document.querySelector('.wrapper');
+const el2 = document.querySelectorAll('meta');
+console.log('el2:', el2);
+console.log('el1:', el1);
+const el3 = document.getElementsByClassName('wrapper');
+const el4 = document.getElementsByTagName('meta');
+console.log('el3:', el3);
+console.log('el4:', el4);
 
-async function generate() {
-	try {
-		wrapper.innerHTML = '';
-		const data = await Promise.all([getActivity(), getActivity(), getActivity()]);
-		console.log('generate  data:', data);
-		for (let el of data) {
-			const element = document.createElement('div');
-			element.innerHTML = `${el.activity}`;
-			wrapper.appendChild(element);
-		}
-	} catch (e) {
-		console.error(e);
-	}
+const button1 = document.createElement('button');
+button1.innerText = 'test1';
+
+const button2 = document.createElement('button');
+button2.innerText = 'test2';
+
+el1.append(button1);
+// el1.prepend(button2);
+// el1.before(button2);
+el1.after(button2);
+
+function generate() {
+	el1.remove();
 }
