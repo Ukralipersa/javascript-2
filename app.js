7a3e7288f5a3d00b5ad9eb9271503d5aa850c7bb
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-24 13:33:30 +0300

    72 - Упражнение "Генератор активностей"

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 'use strict';
 
-async function main() {
-	const res = await fetch('https://dummyjson.com/auth/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			username: 'kminchelle',
-			password: '0lelplR',
-		}),
-	});
-	const data = await res.json();
-	console.log(data);
+/*
+		Сделать генератор 3х идей от скуки
+		https://www.boredapi.com/api/activity
+		с отображением на странице
+*/
+const wrapper = document.querySelector('.wrapper');
+
+async function getActivity() {
+	const res = await fetch('https://www.boredapi.com/api/activity');
+	return res.json();
 }
 
-main();
+async function generate() {
+	try {
+		wrapper.innerHTML = '';
+		const data = await Promise.all([getActivity(), getActivity(), getActivity()]);
+		console.log('generate  data:', data);
+		for (let el of data) {
+			const element = document.createElement('div');
+			element.innerHTML = `${el.activity}`;
+			wrapper.appendChild(element);
+		}
+	} catch (e) {
+		console.error(e);
+	}
+}
