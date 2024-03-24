07822cd3f430e051593e08e01bca259fdaf0d61e
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-24 11:05:01 +0300

    69 - Параллельное выполнение

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 'use strict';
 
-const acyncArrow = async () => {
-	try {
-		const response = await fetch('https://dummyjson.com/products');
-		const data = await response.json();
-		return data;
-	} catch (e) {
-		console.error(e);
-		throw e;
-	}
-};
+async function getAllProducts() {
+	const response = await fetch('https://dummyjson.com/products/');
+	return response.json();
+}
 
-// console.log('1');
-// acyncArrow()
-// 	.then(data => console.log(data))
-// 	.catch(() => console.error(e))
-// 	.finally(() => console.log('2'));
+async function getProduct(id) {
+	const response = await fetch('https://dummyjson.com/products/' + id);
+	return response.json();
+}
 
-(async () => {
-	console.log('1');
-	const res = await acyncArrow();
+async function getProductError(id) {
+	const response = await fetch('https://dummyjson.com/productss/' + id);
+	return response.json();
+}
+
+async function main() {
+	const { products } = await getAllProducts();
+	const res = await Promise.all(products.map(product => getProduct(product.id)));
 	console.log(res);
-	console.log('2');
-})();
+}
+
+main();
