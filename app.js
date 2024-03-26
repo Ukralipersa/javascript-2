ee9f3aa9b2c61eae7027624cc3cb2800426d71a2
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-26 23:17:41 +0300

    80 - Упражнение "Поиск по списку"

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function (e) {
-	console.log('DOMContentLoaded');
-	console.log(e);
-});
+/*
+  Динамически создать N элементов с текстом и поле
+  для поиска. При вводе в поле, выделять элементы,
+  которые содержат введенный текст
+*/
+const wrapper = document.querySelector('.wrapper');
 
-window.addEventListener('load', function (e) {
-	console.log('load');
-	console.log(e);
-});
+for (let i = 0; i < 100; i++) {
+	let el = document.createElement('div');
+	el.innerHTML = i;
+	wrapper.append(el);
+}
 
-window.addEventListener('beforeunload', function (e) {
-	e.preventDefault();
-	e.returnValue = '';
-});
+function search(event) {
+	let inputValue = event.target.value;
+	for (let el of [...wrapper.children]) {
+		if (el.innerHTML.includes(inputValue)) {
+			el.classList.add('yellow');
+			continue;
+		}
+		el.classList.remove('yellow');
+	}
+}
