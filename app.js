38bc361b0b9ab039ca1427a11a87e5c226c8de93
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-27 22:05:58 +0300

    84 - Await на верхнем уровне

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 
-import { add, sub } from './calc.js';
+import { add, res, sub } from './calc.js';
 
 console.log(add(1, 3));
 console.log(sub(1, 3));
+
+console.log(res);
+console.log(1);
