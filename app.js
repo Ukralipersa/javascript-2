23407f98d98e9f45e62bad3edca36c996c2a5167
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-03-27 21:40:34 +0300

    81 - IIFE в модулях

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 'use strict';
 
-/*
-  Динамически создать N элементов с текстом и поле
-  для поиска. При вводе в поле, выделять элементы,
-  которые содержат введенный текст
-*/
-const wrapper = document.querySelector('.wrapper');
-
-for (let i = 0; i < 100; i++) {
-	let el = document.createElement('div');
-	el.innerHTML = i;
-	wrapper.append(el);
-}
-
-function search(event) {
-	let inputValue = event.target.value;
-	for (let el of [...wrapper.children]) {
-		if (el.innerHTML.includes(inputValue)) {
-			el.classList.add('yellow');
-			continue;
-		}
-		el.classList.remove('yellow');
-	}
-}
+(function () {
+	let a = 1;
+	console.log(APP.calc.add(1, 3));
+	console.log(APP.calc.sub(1, 3));
+})();
